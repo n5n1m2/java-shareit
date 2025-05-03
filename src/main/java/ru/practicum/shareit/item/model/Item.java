9e760e7a2ff0109d41841916956c8921c58daddc
n5n1m2 <n5n1m2@mail.ru>
2025-05-04 02:24:13 +0300

    2025-05-04 02:24:02

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.User;
+
+@Data
 public class Item {
+    private Integer id;
+    @NotBlank(message = "The name must not be empty")
+    private String name;
+    @NotBlank(message = "The description must not be empty")
+    private String description;
+    private boolean available;
+    @NotNull(message = "Item must have owner")
+    private User owner;
+    private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ItemRequest {
+    private Integer id;
+    @NotBlank(message = "The description must not be empty")
+    private String description;
+    @NotNull(message = "Item must have requester")
+    private User requester;
+    private LocalDateTime requestDate;
 }
